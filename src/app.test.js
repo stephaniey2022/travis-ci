fd0e6420874a17bc1dc56bd08a604af2ffcada70
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2020-05-25 00:25:30 -0500

    Addressed mutation-observer

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'mutationobserver-shim';
 import { render } from '@testing-library/react';
 import App from './App';
 import Home from './components/Home'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<Home />);
-  const linkElement = getByText(/Freedom Generator/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/Freedom Generator/i);
+  expect(titleElement).toBeInTheDocument();
 });

1fd69106c0de1a856bcaded22cd8b3614a3c108f
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2020-05-26 15:40:23 -0500

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@ import App from './App';
 import Home from './components/Home'
 
 
-global.MutationObserver = window.MutationObserver;
+
 test('renders app', () => {
   const { getByText } = render(<App />);
   const titleElement = getByText(/Freedom Generator/i);

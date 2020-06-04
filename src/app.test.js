0c08b81328e5c4ba99822aff20701931e81cb1c4
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2020-06-03 20:34:35 -0500

    installing and using mutationobserver

--- src/app.test.js
+++ src/app.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import 'mutationobserver-shim';
+global.MutationObserver = window.MutationObserver;
 import Home from './components/Home'
 
 
 
 test('renders app', () => {
   const { getByText } = render(<App />);
-  const titleElement = getByText(/Freedom Generator/i);
-  expect(titleElement).toBeInTheDocument();
+  const linkElement = getByText(/Freedom Generator/i);
+  expect(linkElement).toBeInTheDocument();
 });
